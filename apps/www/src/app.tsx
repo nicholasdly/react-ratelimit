7e850d73c0851c9a3e80376213b8df30ae3bd516
Nicholas Ly <nichdly@gmail.com>
2025-02-17 11:22:15 -0600

    feat: tailwind css

--- apps/www/src/app.tsx
+++ apps/www/src/app.tsx
@@ -3,5 +3,5 @@ import { add } from "@repo/math/add";
 export default function App() {
   const x = add(1, 2);
 
-  return <main>{x}</main>;
+  return <main className="text-2xl font-semibold">{x}</main>;
 }
